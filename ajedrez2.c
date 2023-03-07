59b63849d5090bd8bc5e8350773983f4c3feaf13
Alondra Gómez <alondra.s.gomez3@gmail.com>
2023-03-07 05:31:44 +0000

    Prueba 1 práctica 2

---
+++ ajedrez2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+short opcion=0;
+
+    while(1){
+        printf("\n\t***Ajedrez***\n");
+        printf("¿Que desea realizar?\n");
+
+        printf ("1) Mover Reina.\n");
+      
+        printf ("2) Mover Rey.\n");
+      
+        printf ("3) Salir.\n");
+      
+        scanf ("%hd", &opcion);
+
+    }
+
+}
