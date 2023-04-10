618506485e500045f90bebe615ca04e0d75b3226
Alondra Gómez <alondra.s.gomez3@gmail.com>
2023-04-10 19:18:53 +0000

    cambios práctica 5

--- Práctica5/SistemaDeTurnos.c
+++ Práctica5/SistemaDeTurnos.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-void formarse();
+
+struct Pila {
+	int top;
+	unsigned capacity;
+	int* arreglo;
+};
+
+struct Pila* createStack(unsigned capacity)
+{
+	struct Pila* pila = (struct Pila*)malloc(sizeof(struct Pila));
+	pila->capacity = capacity;
+	pila->top = -1;
+	pila->arreglo = (int*)malloc(pila->capacity * sizeof(int));
+	return pila;
+}
+
+int isFull(struct Pila* pila)
+{
+	return pila->top == pila->capacity -1;
+}
+
+int isEmpty(struct Pila* pila)
+{
+	return pila->top == -1;
+}
+
+void formarse(struct Pila* pila){
+    if(isFull(pila)){
+        printf("Ya no hay turnos disponibles...\n");
+    }else{
+        pila->top++;
+        pila->arreglo[pila->top] = pila->top + 1;
+        printf("El elemento se encuentra formado en la cola con el turno %d.\n", pila->arreglo[pila->top]);
+    }
+}
 
 int main(){
     short opcion = 0;
+    struct Pila* pila = createStack(10);
 
     while (1){
         printf ("\n\t*** Sistema de Turnos ***\n");
       
-        printf ("Elija una opcion: \n");
+        printf ("Opciones disponibles: \n");
       
         printf ("1) Formarse.\n");
       
@@ -19,7 +54,7 @@ int main(){
         switch (opcion){
 	
     case 1:
-    formarse();
+    formarse(pila);
     break;
 	
     case 2:
@@ -33,6 +68,3 @@ int main(){
 
 }
 
-formarse(){
-    return 0;
-}
