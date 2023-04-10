98987ac24722dcedaa5384e73e900ffcb02093f7
Alondra Gómez <alondra.s.gomez3@gmail.com>
2023-04-10 21:28:06 +0000

    cambios practica 5

--- Práctica5/SistemaDeTurnos.c
+++ Práctica5/SistemaDeTurnos.c
@@ -2,59 +2,137 @@
 #include <stdlib.h>
 
 struct Pila {
-	int top;
-	unsigned capacity;
-	int* arreglo;
+    int top;
+    unsigned capacity;
+    int* arreglo;
 };
 
 struct Pila* createStack(unsigned capacity)
 {
-	struct Pila* pila = (struct Pila*)malloc(sizeof(struct Pila));
-	pila->capacity = capacity;
-	pila->top = -1;
-	pila->arreglo = (int*)malloc(pila->capacity * sizeof(int));
-	return pila;
+    struct Pila* pila = (struct Pila*)malloc(sizeof(struct Pila));
+    pila->capacity = capacity;
+    pila->top = -1;
+    pila->arreglo = (int*)malloc(pila->capacity * sizeof(int));
+    return pila;
 }
 
 int isFull(struct Pila* pila)
 {
-	return pila->top == pila->capacity -1;
+    return pila->top == pila->capacity -1;
 }
 
 int isEmpty(struct Pila* pila)
 {
-	return pila->top == -1;
+    return pila->top == -1;
 }
 
-void formarse(struct Pila* pila){
-    if(isFull(pila)){
-        printf("Ya no hay turnos disponibles...\n");
-    }else{
+void push(struct Pila* pila, int item)
+{
+    if (isFull(pila)) {
+        printf("La pila está llena\n");
+    } else {
         pila->top++;
-        pila->arreglo[pila->top] = pila->top + 1;
-        printf("El elemento se encuentra formado en la cola con el turno %d.\n", pila->arreglo[pila->top]);
+        pila->arreglo[pila->top] = item;
+    }
+}
+
+int pop(struct Pila* pila)
+{
+    if (isEmpty(pila)) {
+        printf("La pila está vacía\n");
+        return -1;
+    } else {
+        int item = pila->arreglo[pila->top];
+        pila->top--;
+        return item;
+    }
+}
+
+struct Cola {
+    int front, rear, size;
+    unsigned capacity;
+    int* arreglo;
+};
+
+struct Cola* createQueue(unsigned capacity)
+{
+    struct Cola* cola = (struct Cola*)malloc(sizeof(struct Cola));
+    cola->capacity = capacity;
+    cola->front = 0;
+    cola->rear = -1;
+    cola->size = 0;
+    cola->arreglo = (int*)malloc(cola->capacity * sizeof(int));
+    return cola;
+}
+
+int isQueueFull(struct Cola* cola)
+{
+    return cola->size == cola->capacity;
+}
+
+int isQueueEmpty(struct Cola* cola)
+{
+    return cola->size == 0;
+}
+
+void enqueue(struct Cola* cola, int item)
+{
+    if (isQueueFull(cola)) {
+        printf("La cola está llena\n");
+    } else {
+        cola->rear = (cola->rear + 1) % cola->capacity;
+        cola->arreglo[cola->rear] = item;
+        cola->size++;
+    }
+}
+
+int dequeue(struct Cola* cola)
+{
+    if (isQueueEmpty(cola)) {
+        printf("La cola está vacía\n");
+        return -1;
+    } else {
+        int item = cola->arreglo[cola->front];
+        cola->front = (cola->front + 1) % cola->capacity;
+        cola->size--;
+        return item;
+    }
+}
+
+void formarse(struct Pila* pila,struct Cola* cola)
+{
+    int turno = pop(pila);
+    if (turno != -1) {
+        enqueue(cola, turno);
+        printf("El elemento se encuentra formado en la cola con el turno %d.\n", turno);
+    } else {
+        printf("Ya no hay turnos disponibles...\n");
     }
 }
 
 int main(){
     short opcion = 0;
     struct Pila* pila = createStack(10);
-
+    for (int i = 10; i >= 1; i--) {
+        push(pila, i);
+    }
+    struct Cola* cola = createQueue(10);
+    
     while (1){
         printf ("\n\t*** Sistema de Turnos ***\n");
       
-        printf ("Opciones disponibles: \n");
+        printf ("\tOpciones disponibles: \n");
       
-        printf ("1) Formarse.\n");
+        printf ("\t1) Formarse.\n");
       
-        printf ("2) Salir.\n");
+        printf ("\t2) Salir.\n");
       
         scanf ("%hd", &opcion);
       
         switch (opcion){
 	
     case 1:
-    formarse(pila);
+    formarse(pila, cola);
     break;
 	
     case 2:
@@ -67,4 +145,3 @@ int main(){
     }
 
 }
-
